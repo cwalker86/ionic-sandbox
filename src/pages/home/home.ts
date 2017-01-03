9cd07243249d47707e0c0457bcc2a7590b342061
Cyrus Rhett Walker II <rhettwalker86@gmail.com>
2017-01-03 16:50:41 -0500

    Github get request

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { GitHubService } from '../../providers/git-hub-service';
+// import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [GitHubService]
+
 })
 export class HomePage {
+  public foundRepos;
+  public username;
 
-  constructor(public navCtrl: NavController) {
-    
+  // constructor(public navCtrl: NavController) {
+  // }
+  constructor(private github: GitHubService) {
   }
 
+  getRepos() {
+    this.github.getRepos(this.username).subscribe(
+      data => {
+        this.foundRepos = data.json();
+      },
+      err => console.error(err),
+      () => console.log('getRepos completed')
+    );
+  }
 }
